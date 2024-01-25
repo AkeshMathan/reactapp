582b70400d56ac4fda3e4b778d956316f8a87644
Akesh M <akeshm.21ece@kongu.edu>
2024-01-25 12:12:41 +0530

    v2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
+import PropsComponent from "./components/propComponent";
+import StateComponent from "./components/stateComponet";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <StateComponent />
+        <PropsComponent name="Akesh" course="MERN" />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
---
+++ src/components/propComponent.jsx
@@ -0,0 +1,11 @@
+function PropsComponent(props) {
+  return (
+    <div>
+      <h3>
+        Hai {props.name}
+        <br></br>Welcome to {props.course} class
+      </h3>
+    </div>
+  );
+}
+export default PropsComponent;
